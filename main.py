43cb579f917e9e4290fc86ffb0e6f466c8931e58
asnassif <52229643+asnassif@users.noreply.github.com>
2019-09-10 15:34:30 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("PyBank")
+import os
+import csv
 
+csvpath= os.path.join('..', 'PyBank', 'budget_data.csv')
+
+with open('budget_data.csv', newline='') as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+
+    csv_header= next(csvreader)
+    print(f"CSV Header : {csv_header}")
+
+    for line in csvreader:
+        print((line))
+
+    total_months= csvreader.line_num
+    print("Total_Months:", total_months-1)
+
+
+    csvfile.next()
+    total_money= sum(int(r[1] for r in csv.reader(fin))
+    print(total_money)
